15f51f7b8336ace463a46cb04526b1deb9be90d0
Dmitry Osipa <dima.osipa@gmail.com>
2017-07-30 23:54:29 +0300

    added project skeleton

---
+++ GoEuro/GoEuro/ViewControllers/GETransportSelectViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GETransportSelectViewController.swift
+//  GoEuro
+//
+//  Created by Dmitry Osipa on 7/30/17.
+//  Copyright © 2017 Dmitry Osipa. All rights reserved.
+//
+
+import UIKit
+
+class GETransportSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
