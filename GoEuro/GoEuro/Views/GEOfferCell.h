f3ff65828cc58777fbca6149b8828117350f926c
Dmitry Osipa <dima.osipa@gmail.com>
2017-07-31 03:02:24 +0300

    added cell class to display info

---
+++ GoEuro/GoEuro/Views/GEOfferCell.h
@@ -0,0 +1,13 @@
+//
+//  GEOfferCell.h
+//  GoEuro
+//
+//  Created by Dmitry Osipa on 7/31/17.
+//  Copyright © 2017 Dmitry Osipa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GEOfferCell : UITableViewCell
+
+@end
---
+++ GoEuro/GoEuro/Views/GEOfferCell.m
@@ -0,0 +1,24 @@
+//
+//  GEOfferCell.m
+//  GoEuro
+//
+//  Created by Dmitry Osipa on 7/31/17.
+//  Copyright © 2017 Dmitry Osipa. All rights reserved.
+//
+
+#import "GEOfferCell.h"
+
+@implementation GEOfferCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
